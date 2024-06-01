6ead1de5b7f541cd0b7c939b9536bb222696e25c
SpiralP <spiral.palmer@gmail.com>
2024-05-31 20:19:38 -0700

    update-nix-hashes: also check if strings are empty

--- flake.nix
+++ flake.nix
@@ -69,11 +69,19 @@
               NEW_HASH="$(prefetch-npm-deps "$PACKAGE_LOCK_PATH" 2>/dev/null)"
 
               echo "$OLD_HASH" "$NEW_HASH"
+              test -z "$OLD_HASH" && exit 1
+              test -z "$NEW_HASH" && exit 1
               test "$OLD_HASH" = "$NEW_HASH" && exit 0
 
-              grep -q "$OLD_HASH" flake.nix || { echo "couldn't find old hash in flake.nix"; exit 1; }
+              if ! grep -q "$OLD_HASH" flake.nix; then
+                echo "couldn't find old hash in flake.nix"
+                exit 1
+              fi
               sd --fixed-strings "$OLD_HASH" "$NEW_HASH" flake.nix
-              grep -q "$NEW_HASH" flake.nix || { echo "couldn't find new hash in flake.nix"; exit 1; }
+              if ! grep -q "$NEW_HASH" flake.nix; then
+                echo "couldn't find new hash in flake.nix"
+                exit 1
+              fi
             '';
           };
         }
